dd8e4ab8dd1c063391da0aa55bbed142cfaac1a7
Quentin EPPE <q.eppe@student.helmo.be>
2023-01-07 16:50:19 +0100

    WebpageView simplified + display the url in the app bar

--- lib/src/webpage_view.dart
+++ lib/src/webpage_view.dart
@@ -10,27 +10,12 @@ class WebpageView extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            // Update loading bar.
-          },
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {},
-          onWebResourceError: (WebResourceError error) {},
-          onNavigationRequest: (NavigationRequest request) {
-            if (request.url.startsWith('https://www.youtube.com/')) {
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
       ..loadRequest(Uri.parse(url));
 
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(url),
+      ),
       body: WebViewWidget(controller: controller),
     );
   }
